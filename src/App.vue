42b511ca0e53ef7aa46ffae37a3b5fb03e9f2dae
mandril888 <arturo.rubio.89.8@gmail.com>
2022-12-14 18:14:44 +0100

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+    <div>
+      <p>{{ joke.setup }}</p>
+      <p v-show="!isDelivery"><button @click="isDelivery = true">Tell Me!</button></p>
+      <div v-show="isDelivery">
+        <p>{{ joke.delivery }}</p>
+        <p><button @click="getJoke">Another</button></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+
+const joke = ref({})
+const isDelivery = ref(false)
+
+
+const getJoke = () => {
+  fetch('https://v2.jokeapi.dev/joke/christmas')
+    .then(res => res.json())
+    .then(data => {
+      joke.value = data
+      isDelivery.value = false
+    })
+}
+getJoke()
 </script>
+
+<style>
+p {
+  text-align: center;
+}
+</style>
